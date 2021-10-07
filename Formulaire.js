f10489435bc31ab550c3ec7f424f68a2b2e0960f
megdoud22 <fatah.megdoud@yahoo.fr>
2021-10-07 20:57:38 +0200

    YES

--- Formulaire.js
+++ Formulaire.js
@@ -1,5 +1,9 @@
 
-  // Validation de formilaire avec les evenements 
+  // Validation de formilaire avec les evenements en java-script
+
+
+
+
 
 
      
@@ -9,13 +13,36 @@
      e.preventDefault();
         
      var erreur;
+
      var nom = document.getElementById("nom");
+     var prenom = document.getElementById("prenom");
+     var date = document.getElementById("date");
+     var email = document.getElementById("email");
+     var telephone = document.getElementById("telephone");
+     var adresse = document.getElementById("adresse");
+     var postale = document.getElementById("postal");
+     var message = document.getElementById("precisions");
+
+
     
      if (!nom.value) {
 
        erreur = "Veulillez saisir votre nom dans le champ de saisie de formulaire !";
 
      }
+
+     if (!prenom.value) {
+
+      erreur = "Veuillez saisir votre prenom dans le champ de saisie de formulaire !"
+
+     }
+
+     if (!date.value) {
+      
+      erreur = "Veuillez saisir votre date de naissance dans le champ de saisie de formulaire !"
+      
+     }
+
 
      
      
