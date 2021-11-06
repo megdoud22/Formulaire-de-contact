46ba8de8f3d07bf1bf88e6efdf51af03e72598a5
megdoud22 <fatah.megdoud@yahoo.fr>
2021-11-06 10:38:55 +0100

    BBBB

--- Formulaire-cible.php
+++ Formulaire-cible.php
@@ -1,45 +1,5 @@
- var Prenom = document.getElementById("prenom");
-     var laDate = document.getElementById("date");
-     var Email = document.getElementById("email");
-     var telephoneNumber = document.getElementById("telephone");
-     var Adress = document.getElementById("adresse");
-     var PostalCode = document.getElementById("postal");
+    
 
 
 
-
-     if (!prenom.value) {
-
-        error = "Veulillez saisir votre prenom dans le champ de saisie de formulaire !";
-        
-     }
-     if (!date.value) {
-
-        error = "Veulillez saisir votre date dans le champ de saisie de formulaire !";
-        
-     }
-     if (!email.value) {
-
-        error = "Veulillez saisir votre émail dans le champ de saisie de formulaire !";
-        
-     }
-     if (!telephone.value) {
-
-        error = "Veulillez saisir votre telephone dans le champ de saisie de formulaire !";
-        
-     }
-     if (!nom.value) {
-
-        error = "Veulillez saisir votre nom dans le champ de saisie de formulaire !";
-        
-     }
-     if (!adresse.value) {
-
-        error = "Veulillez saisir votre adresse dans le champ de saisie de formulaire !";
-        
-     }
-     if (!code-postal.value) {
-
-        error = "Veulillez saisir votre code postale dans le champ de saisie de formulaire !";
-        
-     }
+     
--- Formulaire.js
+++ Formulaire.js
@@ -1,19 +1,98 @@
 
-  // Validation de formilaire avec les evenements en java-script
+
+
+document.getElementById("email2").addEventListener("input", function() {
+
+   var paragrapheErreur = document.getElementById("erreur");
+
+   if (this.value != document.getElementById("email").value) {
+
+      paragrapheErreur.innerHTML = "Les deux adresses émail ne correspondent pas !";
+
+   } else {
+
+      paragrapheErreur.innerHTML = "";
+   }
+
+});
+
+document.Forms["inscription"].addEventListener("submit", function(e) {
+
+   var erreur;
+
+   var inputs = this;
+
+//Traitement cas par cas (input unique)
+
+if (inputs["email"].value != "fatah.megdoud@yahoo.fr") {
+
+   erreur = "Votre adresse émail est incorrect";
+}
+
+// Traitement générique
+for (var i = 0; i < inputs.length; i++) {
+   console.log(inputs[i]);
+   if (!inputs[i].value) {
+
+      erreur ="Veuillez remplir tous les champs";
+      break;
+   }
+}
+
+if (erreur) {
+
+      e.preventDefault();
+      
+      document.getElementById("erreur").innerHTML = erreur;
+
+      return false;
+
+      } else {
+
+        alert('Votre formulaire a été bien envoyé ! Merci pour votre confiance');
+     }
+});
 
 
 
 
+
+
+// Validation de formulaire avec les évenements en java-script
+
+
+document.getElementById("inscription").addEventListener("submit", function(e) {
+
+var erreur;
+
+var inputs = this.getElementsByTagName("input");
+
+for (var i = 0; i < inputs.length; i++) {
+
+   console.log(inputs[i]);
+
+   if (!inputs[i].value) {
+
+         erreur = "Veuillez remplir tous les chmaps de saisie !!";
+       }
+
+     }
+
+   if (erreur) {
+
+      e.preventDefault();
+
+      document.getElementById("erreur").innerHTML = erreur;
+      return false;
+
+      } else {
+         alert('Votre formulaire a été bien envoyé ! Merci pour votre confiance');
+     }
 
 
      
 
-     document.getElementById("inscription").addEventListener("submit", function(e) {
-     
-     e.preventDefault();
-        
-     var erreur;
-
+/* 
      var nom = document.getElementById("nom");
      var prenom = document.getElementById("prenom");
      var date = document.getElementById("date");
@@ -24,42 +103,70 @@
      var message = document.getElementById("precisions");
 
 
-    
+
      if (!nom.value) {
 
-       erreur = "Veulillez saisir votre nom dans le champ de saisie de formulaire !";
-
+        erreur = "Veulillez saisir votre nom dans le champ de saisie de formulaire !";
+        
      }
-
+     
      if (!prenom.value) {
 
-      erreur = "Veuillez saisir votre prenom dans le champ de saisie de formulaire !"
-
+        erreur = "Veulillez saisir votre prenom dans le champ de saisie de formulaire !";
+        
      }
 
      if (!date.value) {
-      
-      erreur = "Veuillez saisir votre date de naissance dans le champ de saisie de formulaire !"
-      
+
+        erreur = "Veulillez saisir votre date dans le champ de saisie de formulaire !";
+        
+     }
+     if (!email.value) {
+
+        error = "Veulillez saisir votre émail dans le champ de saisie de formulaire !";
+        
+     }
+     if (!telephone.value) {
+
+        error = "Veulillez saisir votre telephone dans le champ de saisie de formulaire !";
+        
+     }
+     
+     if (!adresse.value) {
+
+        error = "Veulillez saisir votre adresse dans le champ de saisie de formulaire !";
+        
      }
 
+      if (!postale.value) {
 
-     
-     
+        error = "Veulillez saisir votre adresse dans le champ de saisie de formulaire !";
+        
+     }
 
-     if (erreur) {
-        e.preventDefault();
-        document.getElementById("erreur").innerHTML = erreur;
-        return false;
-     } else {
+      if (!message.value) {
+
+        error = "Veulillez saisir votre adresse dans le champ de saisie de formulaire !";
+        
+     }
+
+   
+
+
+      if (erreur) {
+
+      e.preventDefault();
+
+      document.getElementById("erreur").innerHTML = erreur;
+      return false;
+      } else {
 
         alert('Votre formulaire a été bien envoyé ! Merci pour votre confiance');
-
-    }
+     }
 
      
     });
-
+*/
 
 
      
